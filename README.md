2714974d9d87f20d3da0fb64c2d61c5a61d30a38
Ryan Sullivan <ryan@ryansully.com>
2017-05-21 21:39:52 -0700

    Bootstrap and configure CRA

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 Firecards
 =========
+
+A party game for horrible people. Powered by Firebase.
+
+This project was bootstrapped with [Create React App](https://github.com/facebookincubator/create-react-app).
+See the [CRA User Guide](https://github.com/facebookincubator/create-react-app/blob/master/packages/react-scripts/template/README.md) for details.
---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react'
+import './App.css'
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header>
+          <img src={process.env.PUBLIC_URL + '/static/images/icons/icon-192x192.png'} alt="logo" className="logo" />
+          <h2>Firecards</h2>
+        </header>
+        <p className="intro">
+          A party game for horrible people, powered by Firebase.
+        </p>
+      </div>
+    )
+  }
+}
+
+export default App
