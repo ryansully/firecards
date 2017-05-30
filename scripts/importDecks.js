504e0d714e06e6aaacdbe9e06cbb960c1664fc88
Ryan Sullivan <ryan@ryansully.com>
2017-05-30 03:11:51 -0700

    Import decks during postinstall

---
+++ scripts/importDecks.js
@@ -0,0 +1,27 @@
+// import decks to Firebase database
+const async = require('async')
+const exec = require('child_process').exec
+const fs = require('fs')
+const path = require('path')
+
+const deckPath = '/decks'
+const dirPath = `data${deckPath}`
+
+function main() {
+  fs.readdir(dirPath, (err, files) => {
+    async.parallel(files.map((file) => () => {
+      const dbPath = `${deckPath}/${path.basename(file, '.json')}`
+      const infile = `${dirPath}/${file}`
+      const command = `firebase database:set -j -y ${dbPath} ${infile}`
+      console.log(`Importing ${infile}...`)
+      exec(command, (error, stdout, stderr) => {
+        if (stdout) {
+          console.log(JSON.parse(stdout).status)
+        }
+      })
+    }))
+  })
+}
+
+module.exports = main
+if (require.main === module) { main() }
--- scripts/postinstall.js
+++ scripts/postinstall.js
@@ -1,1 +1,2 @@
+require('./importDecks')()
 require('./writeEnv')()
