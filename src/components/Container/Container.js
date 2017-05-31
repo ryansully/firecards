f113ff9cfbae350b696bbf4d18d40a2517a9c82b
Ryan Sullivan <ryan@ryansully.com>
2017-05-30 22:32:05 -0700

    Add Loader and Dimmer to Container

--- src/components/Container/Container.js
+++ src/components/Container/Container.js
@@ -1,14 +1,20 @@
 import React from 'react'
-import { Container as UIContainer } from 'semantic-ui-react'
+import { Container as UIContainer, Dimmer, Loader } from 'semantic-ui-react'
 import Navigation from '../Navigation/Navigation'
+import './Container.css'
 
-const Container = ({ children, activeNavItem }) => {
+const Container = ({ children, loading = false }) => {
   return (
     <div className="Container">
-      <Navigation />
-      <UIContainer>
-        <div>{children}</div>
-      </UIContainer>
+      <Dimmer.Dimmable dimmed={loading}>
+        <Navigation />
+        <Dimmer active={loading}>
+          <Loader />
+        </Dimmer>
+        <UIContainer>
+          <div>{children}</div>
+        </UIContainer>
+      </Dimmer.Dimmable>
     </div>
   )
 }
