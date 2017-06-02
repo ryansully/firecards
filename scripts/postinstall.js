77462fc0f064abc1ac61d834f33961158b89d15d
Ryan Sullivan <ryan@ryansully.com>
2017-06-02 02:06:28 -0700

    Move postinstall shell commands to script

--- scripts/postinstall.js
+++ scripts/postinstall.js
@@ -1,2 +1,10 @@
+const async = require('async')
+const exec = require('child_process').exec
+
+async.series([
+  async.apply(exec, 'firebase login'),
+  async.apply(exec, 'firebase use --add')
+])
+
 require('./importDecks')()
 require('./writeEnv')()
