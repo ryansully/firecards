0459735393074dca37126bf1ee521760fa7289a0
Ryan Sullivan <ryan@ryansully.com>
2017-06-04 16:31:22 -0700

    Rename store.js to configureStore.js

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import 'semantic-ui-css/semantic.min.css'
 import './index.css'
 
 import { Provider } from 'react-redux'
-import configureStore from './store'
+import configureStore from './configureStore'
 import rootSaga from './sagas'
 
 const store = configureStore()
