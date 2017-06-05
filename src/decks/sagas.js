3e03124fe66c19a25ee6d89326261749c57c185c
Ryan Sullivan <ryan@ryansully.com>
2017-06-05 03:17:54 -0700

    Add sagas to toggle selected decks

--- src/decks/sagas.js
+++ src/decks/sagas.js
@@ -1,10 +1,10 @@
-import { all, call, fork, put, take } from 'redux-saga/effects'
-import { actions, ActionTypes } from './dux'
+import { all, call, fork, put, select, take } from 'redux-saga/effects'
+import { actions, ActionTypes, selectors } from './dux'
 import { app, FirebaseSagaHelper } from '../firebase'
 
 const helper = new FirebaseSagaHelper(app)
 
-export function* getDeckIndex() {
+function* getDeckIndex() {
   while (true) {
     yield take(ActionTypes.DECK_INDEX_REQUEST)
     const deckIndex = yield call(helper.get, '/decks/!index')
@@ -14,8 +14,61 @@ export function* getDeckIndex() {
   }
 }
 
+function* setSelectedDecks(decks) {
+  yield put(actions.setSelectedDecks(decks))
+}
+
+function* toggleAllSelected() {
+  while (true) {
+    const action = yield take(ActionTypes.SELECT_ALL_TOGGLE)
+    const decks = yield select(selectors.getDeckOrder)
+    yield call(setSelectedDecks, action.checked ? decks : [])
+  }
+}
+
+function* toggleOfficialSelected() {
+  while (true) {
+    const action = yield take(ActionTypes.SELECT_OFFICIAL_TOGGLE)
+    const decks = yield select(selectors.getOfficialDecks)
+    yield call(setSelectedDecks, action.checked ? decks : [])
+  }
+}
+
+function* toggleSelectedDeck() {
+  while (true) {
+    const action = yield take(ActionTypes.SELECT_DECK_TOGGLE)
+    const selectedDecks = yield select(selectors.getSelectedDecks)
+    if (action.checked) {
+      const deckOrder = yield select(selectors.getDeckOrder)
+
+      // add deck according to deck order
+      yield call(setSelectedDecks, deckOrder.reduce((a, b) => {
+        // push deck to new array if it exists in the previous array
+        if (selectedDecks.includes(b)) { a.push(b) }
+        // push new deck to array
+        if (action.value === b) { a.push(b) }
+        return a
+      }, []))
+    } else {
+      // remove deck from selected list
+      yield call(setSelectedDecks, selectedDecks.filter(id => id !== action.value))
+    }
+  }
+}
+
+export const sagas = {
+  getDeckIndex,
+  setSelectedDecks,
+  toggleAllSelected,
+  toggleOfficialSelected,
+  toggleSelectedDeck,
+}
+
 export default function* root() {
   yield all([
-    fork(getDeckIndex),
+    fork(sagas.getDeckIndex),
+    fork(sagas.toggleAllSelected),
+    fork(sagas.toggleOfficialSelected),
+    fork(sagas.toggleSelectedDeck),
   ])
 }
--- src/decks/sagas.test.js
+++ src/decks/sagas.test.js
@@ -1,20 +1,18 @@
-import { all, fork, put, take } from 'redux-saga/effects'
-import { actions, ActionTypes } from './dux'
-import root, { getDeckIndex } from './sagas'
-// import { get } from '../firebase/database'
+import { all, call, fork, put, select, take } from 'redux-saga/effects'
+import { cloneableGenerator } from 'redux-saga/utils'
+import { actions, ActionTypes, selectors } from './dux'
+import root, { sagas } from './sagas'
 
 describe('getDeckIndex saga', () => {
-  const generator = getDeckIndex()
+  const generator = sagas.getDeckIndex()
 
   it('waits for deck index requests', () => {
     expect(generator.next().value).toEqual(take(ActionTypes.DECK_INDEX_REQUEST))
   })
 
   it('gets a deck index from the database', () => {
-    // FIXME: this test fails: "Compared values have no visual difference."
-    // const boundGet = get.bind(this)
-    // expect(generator.next().value).toEqual(call(boundGet, '/decks/!index'))
-    generator.next()
+    const nextValue = generator.next().value
+    expect(nextValue.CALL.args).toEqual(['/decks/!index'])
   })
 
   it('dispatches an action that stores a deck list', () => {
@@ -26,12 +24,136 @@ describe('getDeckIndex saga', () => {
   })
 })
 
+describe('setSelectedDecks saga', function () {
+  const decks = ['test1', 'test2']
+  const generator = sagas.setSelectedDecks(decks)
+
+  it('dispatches an action that sets selected decks', () => {
+    expect(generator.next().value).toEqual(put(actions.setSelectedDecks(decks)))
+  })
+})
+
+describe('toggleAllSelected saga', function () {
+  const state = {decks: {deckOrder: ['test1', 'test2']}}
+  const decks = selectors.getDeckOrder(state)
+  const data = {}
+  data.gen = cloneableGenerator(sagas.toggleAllSelected)()
+
+  it('waits for action to toggle all decks to be selected', () => {
+    expect(data.gen.next().value).toEqual(take(ActionTypes.SELECT_ALL_TOGGLE))
+  })
+
+  const checkedAction = actions.toggleAllSelected(null, {checked: true})
+  const uncheckedAction = actions.toggleAllSelected(null, {checked: false})
+
+  it('selects deck order from state', () => {
+    data.clone = data.gen.clone()
+    expect(data.gen.next(checkedAction).value)
+      .toEqual(select(selectors.getDeckOrder))
+    expect(data.clone.next(uncheckedAction).value)
+      .toEqual(select(selectors.getDeckOrder))
+  })
+
+  it('calls setSelectedDecks saga with checked value', () => {
+    expect(data.gen.next(decks).value)
+      .toEqual(call(sagas.setSelectedDecks, decks))
+  })
+
+  it('calls setSelectedDecks saga with unchecked value', () => {
+    expect(data.clone.next(decks).value)
+      .toEqual(call(sagas.setSelectedDecks, []))
+  })
+})
+
+describe('toggleOfficialSelected saga', function () {
+  const state = {decks: {
+    deckList: {deck1: {name: 'deck1'}, '[': {name: '['}},
+    deckOrder: ['deck1', '[']
+  }}
+  const decks = selectors.getOfficialDecks(state)
+  const data = {}
+  data.gen = cloneableGenerator(sagas.toggleOfficialSelected)()
+
+  it('waits for action to toggle official decks to be selected', () => {
+    expect(data.gen.next().value)
+      .toEqual(take(ActionTypes.SELECT_OFFICIAL_TOGGLE))
+  })
+
+  const checkedAction = actions.toggleOfficialSelected(null, {checked: true})
+  const uncheckedAction = actions.toggleOfficialSelected(null, {checked: false})
+
+  it('selects official decks from state', () => {
+    data.unchecked = data.gen.clone()
+    expect(data.gen.next(checkedAction).value)
+      .toEqual(select(selectors.getOfficialDecks))
+    expect(data.unchecked.next(uncheckedAction).value)
+      .toEqual(select(selectors.getOfficialDecks))
+  })
+
+  it('calls setSelectedDecks saga with checked value', () => {
+    expect(data.gen.next(decks).value)
+      .toEqual(call(sagas.setSelectedDecks, decks))
+  })
+
+  it('calls setSelectedDecks saga with unchecked value', () => {
+    expect(data.unchecked.next(decks).value)
+      .toEqual(call(sagas.setSelectedDecks, []))
+  })
+})
+
+describe('toggleSelectedDeck saga', function () {
+  const data = {}
+  data.gen = cloneableGenerator(sagas.toggleSelectedDeck)()
+
+  it('waits for action to toggle a single deck to be selected', () => {
+    expect(data.gen.next().value).toEqual(take(ActionTypes.SELECT_DECK_TOGGLE))
+  })
+
+  const checkedAction = actions.toggleSelectedDeck(null, {
+    checked: true,
+    value: 'deck2'
+  })
+  const uncheckedAction = actions.toggleSelectedDeck(null, {
+    checked: false,
+    value: 'deck2'
+  })
+
+  it('selects selected decks from state', () => {
+    data.unchecked = data.gen.clone()
+    expect(data.gen.next(checkedAction).value)
+      .toEqual(select(selectors.getSelectedDecks))
+    expect(data.unchecked.next(uncheckedAction).value)
+      .toEqual(select(selectors.getSelectedDecks))
+  })
+
+  it('removes deck from selected list with unchecked value', () => {
+    const nextValue = data.unchecked.next(['deck1', 'deck2']).value
+    expect(nextValue.CALL.args).toEqual([['deck1']])
+    expect(nextValue.CALL.fn).toEqual(sagas.setSelectedDecks)
+  })
+
+  it('selects deck order from state with checked value', () => {
+    expect(data.gen.next(['deck1']).value)
+      .toEqual(select(selectors.getDeckOrder))
+  })
+
+  const deckOrder = ['deck1', 'deck2']
+
+  it('adds deck according to deck order', () => {
+    expect(data.gen.next(deckOrder).value)
+      .toEqual(call(sagas.setSelectedDecks, deckOrder))
+  })
+})
+
 describe('root saga', () => {
   const generator = root()
 
   it('yields an array of sagas', () => {
     expect(generator.next().value).toEqual(all([
-      fork(getDeckIndex),
+      fork(sagas.getDeckIndex),
+      fork(sagas.toggleAllSelected),
+      fork(sagas.toggleOfficialSelected),
+      fork(sagas.toggleSelectedDeck),
     ]))
   })
 })
