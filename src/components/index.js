0309465caa9c277757aee41bb36708b29c475a73
Ryan Sullivan <ryan@ryansully.com>
2017-06-05 03:37:10 -0700

    Export withDecks HOC in component index

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,4 @@
 export { default as Dashboard } from './Dashboard'
 export { default as FirebaseUIAuth } from './FirebaseUIAuth'
 export { default as PageContainer } from './PageContainer/PageContainer'
+export { default as withDecks } from './decks/withDecks'
