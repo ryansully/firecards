3dbf8e3c19edcfd5cf14b3621e11ff25c257b353
Ryan Sullivan <ryan@ryansully.com>
2017-06-06 19:54:40 -0700

    Write better tests for NewGame route

--- src/routes/NewGame/NewGame.test.js
+++ src/routes/NewGame/NewGame.test.js
@@ -1,13 +1,22 @@
 import React from 'react'
 import { shallow } from 'enzyme'
 import { NewGame } from './NewGame'
-import { DeckSelector } from '../../components'
+import { PageContainer } from '../../components'
 
 it('renders without crashing', () => {
   shallow(<NewGame />)
 })
 
-it('renders the deck list', () => {
+it('shows a loading page container with no deck list', () => {
   const wrapper = shallow(<NewGame />)
-  expect(wrapper.find(DeckSelector).length).toEqual(1)
+  const container = wrapper.find(PageContainer)
+  expect(container.length).toEqual(1)
+  expect(container.prop('loading')).toEqual(true)
+})
+
+it('hides the page container loader when there is deck list and order', () => {
+  const wrapper = shallow(<NewGame deckList={{}} deckOrder={[]} />)
+  const container = wrapper.find(PageContainer)
+  expect(container.length).toEqual(1)
+  expect(container.prop('loading')).toEqual(false)
 })
