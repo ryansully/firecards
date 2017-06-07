1c36aa2ff73f668b236a2215374ac6fbcf794845
Ryan Sullivan <ryan@ryansully.com>
2017-06-06 18:12:47 -0700

    Simplify adding users to database

--- functions/auth.js
+++ functions/auth.js
@@ -6,28 +6,23 @@ module.exports = (admin) => {
 
     return admin.database().ref('admins').once('value')
       .then((snapshot) => {
+        const providerUid = user.providerData[0].uid
+
         // is user an admin?
         console.log('Checking /admins...')
-        if (user.providerData[0].uid in snapshot.val()) {
+        if (providerUid in snapshot.val()) {
           // add matching providerUid to /admins
-          console.log('DB ADMIN ADD', user.uid)
-          return snapshot.ref.update({[user.uid]: true})
-        } else {
-          // is user an invited guest?
-          console.log('Checking /guests...')
-          return admin.database().ref('guests').once('value')
+          console.log('DB ADMIN ADD', user)
+          snapshot.ref.update({[user.uid]: true})
         }
-      })
-      .then((snapshot) => {
-        // snapshot is falsy if user is an admin (from the previous step)
-        if (!snapshot || (snapshot.val() && user.email in snapshot.val())) {
-          // add user to database if admin or invited guest
-          return addUserToDatabase(user)
-        } else {
-          // user was not invited, so delete user
-          console.log('AUTH USER DELETE', user)
-          return admin.auth().deleteUser(user.uid)
-        }
+
+        // add user to /users
+        console.log('DB USER ADD', user)
+        return admin.database().ref('users/' + user.uid).set({
+          displayName: user.displayName,
+          photoURL: user.photoURL,
+          providerUid,
+        })
       })
   })
 
@@ -38,18 +33,9 @@ module.exports = (admin) => {
     console.log('DB USER DELETE', user)
     const db = admin.database()
     db.ref('/users/' + user.uid).remove()
+    db.ref('/guests/' + user.uid).remove()
     db.ref('/admins/' + user.uid).remove()
   })
-
-  function addUserToDatabase(user) {
-    // add user to /users
-    console.log('DB USER ADD', user)
-    return admin.database().ref('users/' + user.uid).set({
-      displayName: user.displayName,
-      photoURL: user.photoURL,
-      providerUid: user.providerData[0].uid,
-    })
-  }
 
   return {
     addAuthUserToDatabase,
