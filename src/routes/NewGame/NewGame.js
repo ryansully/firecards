ed160ed304b9fc9d90c8ff8ecd35fb6cf43c5e41
Ryan Sullivan <ryan@ryansully.com>
2017-06-08 06:42:33 -0700

    Implement logic for NewGame page

--- src/routes/NewGame/NewGame.js
+++ src/routes/NewGame/NewGame.js
@@ -1,19 +1,39 @@
-import React from 'react'
+import React, { Component } from 'react'
+import { Redirect } from 'react-router-dom'
 import { Button, Header } from 'semantic-ui-react'
-import { DeckSelector, PageContainer, withDecks } from '../../components'
+import { DeckSelector, PageContainer, withCurrentGame, withDecks } from '../../components'
 
-export const NewGame = (props) => {
-  return (
-    <PageContainer className="NewGame" loading={!(props.deckList && props.deckOrder)}>
-      <Header as="h2" icon="add" content="New Game" />
-      <DeckSelector {...props} submitButton={<Button content="Create Game" onClick={handleSubmit} />} />
-    </PageContainer>
-  )
+export class NewGame extends Component {
+  state = {
+    gameCreated: false
+  }
+
+  shouldComponentUpdate(nextProps, nextState) {
+    if (!nextProps.deckList || !nextProps.deckOrder) { return false }
+    return true
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault()
+    this.props.createGameRequest(this.props.selectedDecks)
+    this.setState({gameCreated: true})
+  }
+
+  render() {
+    const { props } = this
+    return (
+      <div>
+        {this.state.gameCreated && props.currentGame ? (
+          <Redirect to={`/game/${props.currentGame.key}`} />
+        ) : (
+          <PageContainer className="NewGame" loading={!(props.deckList && props.deckOrder)}>
+            <Header as="h2" icon="add" content="New Game" />
+            <DeckSelector {...props} submitButton={<Button content="Create Game" onClick={this.handleSubmit} />} />
+          </PageContainer>
+        )}
+      </div>
+    )
+  }
 }
 
-export function handleSubmit(event) {
-  // event.preventDefault()
-  // TODO: dispatch action to create new game
-}
-
-export default withDecks(NewGame)
+export default withCurrentGame(withDecks(NewGame))
--- src/routes/NewGame/NewGame.test.js
+++ src/routes/NewGame/NewGame.test.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { shallow } from 'enzyme'
+import { Redirect } from 'react-router-dom'
 import { NewGame } from './NewGame'
 import { PageContainer } from '../../components'
 
@@ -19,4 +20,31 @@ it('hides the page container loader when there is deck list and order', () => {
   const container = wrapper.find(PageContainer)
   expect(container.length).toEqual(1)
   expect(container.prop('loading')).toEqual(false)
+})
+
+it('handles submit button click', () => {
+  const createGameRequest = jest.fn()
+  const preventDefault = jest.fn()
+  const wrapper = shallow(<NewGame deckList={{}} deckOrder={[]}
+    createGameRequest={createGameRequest} />)
+
+  wrapper.instance().handleSubmit({preventDefault})
+  expect(preventDefault).toBeCalled()
+  expect(createGameRequest).toBeCalled()
+  expect(wrapper.state('gameCreated')).toEqual(true)
+})
+
+it('redirects after game is created', () => {
+  const createGameRequest = jest.fn()
+  const wrapper = shallow(<NewGame deckList={{}} deckOrder={[]} currentGame={{}}
+    createGameRequest={createGameRequest} />)
+
+  wrapper.instance().setState({gameCreated: true})
+  expect(wrapper.find(Redirect).length).toEqual(1)
+})
+
+it('does not update if there is no deck list or order', () => {
+  const deckList = {}
+  const wrapper = shallow(<NewGame deckList={deckList} />)
+  expect(wrapper.instance().shouldComponentUpdate({deckList})).toEqual(false)
 })
