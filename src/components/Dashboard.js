6da0f73feb0a7b6ade12c9931b91b2824bb714dd
Ryan Sullivan <ryan@ryansully.com>
2017-06-09 06:24:03 -0700

    Only show New Game button for admins and guests

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,14 +1,18 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import { Button, Header } from 'semantic-ui-react'
+import { withCurrentUser } from '../user/components'
 
-const Dashboard = () => {
+export const Dashboard = ({ currentUser }) => {
+  const canCreateGame = currentUser && (currentUser.isAdmin || currentUser.isGuest)
   return (
     <div className="Dashboard">
       <Header as="h2" icon="game" content="Games" />
+      {canCreateGame &&
       <Button as={Link} to="/game/new" icon="add" content="New Game" />
+      }
     </div>
   )
 }
 
-export default Dashboard
+export default withCurrentUser(Dashboard)
--- src/components/Dashboard.test.js
+++ src/components/Dashboard.test.js
@@ -1,7 +1,17 @@
 import React from 'react'
 import { shallow } from 'enzyme'
-import Dashboard from './Dashboard'
+import { Button } from 'semantic-ui-react'
+import { Dashboard } from './Dashboard'
 
 it('renders without crashing', () => {
   shallow(<Dashboard />)
+})
+
+it('only shows New Game button if user is admin or guest', () => {
+  const currentUser = {
+    isAdmin: false,
+    isGuest: true,
+  }
+  const wrapper = shallow(<Dashboard currentUser={currentUser} />)
+  expect(wrapper.find(Button).length).toEqual(1)
 })
