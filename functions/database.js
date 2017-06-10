67300c3fcaff509dc01da492d1a65eb2d757e6fe
Ryan Sullivan <ryan@ryansully.com>
2017-06-09 18:05:25 -0700

    Add database trigger to add game to user

--- functions/database.js
+++ functions/database.js
@@ -1,17 +1,34 @@
 const functions = require('firebase-functions')
 
+const setGameToUser = functions.database.ref('/games/{game_key}')
+  .onWrite((event) => {
+    const gameKey = event.params.game_key
+    const game = event.data.val() || event.data.previous.val()
+    const uid = game.host
+    const value = event.data.exists() || null
+
+    return getUser(event, uid)
+      .then((snapshot) => {
+        // exit if user is not in database
+        if (!snapshot.exists()) { return }
+
+        console.log('USER GAME SET', uid, game)
+        return snapshot.ref.child('games').update({[gameKey]: value})
+      })
+  })
+
 const setUserAsAdmin = functions.database.ref('/admins/{uid}')
   .onWrite((event) => {
     const uid = event.params.uid
     const isAdmin = event.data.exists() || null
 
     // set user as admin or remove admin flag if uid is deleted from /admins
-    return event.data.ref.root.child('users/' + uid).once('value')
+    return getUser(event, uid)
       .then((snapshot) => {
         // exit if user is not in database
         if (!snapshot.exists()) { return }
 
-        console.log('SET USER AS ADMIN', uid, isAdmin)
+        console.log('USER ADMIN SET', uid, isAdmin)
         return snapshot.ref.update({isAdmin})
       })
   })
@@ -22,17 +39,22 @@ const setUserAsGuest = functions.database.ref('/guests/{uid}')
     const isGuest = event.data.exists() || null
 
     // set user as guest or remove guest flag if uid is deleted from /guests
-    return event.data.ref.root.child('users/' + uid).once('value')
+    return getUser(event, uid)
       .then((snapshot) => {
         // exit if user is not in database
         if (!snapshot.exists()) { return }
 
-        console.log('SET USER AS GUEST', uid, isGuest)
+        console.log('USER GUEST SET', uid, isGuest)
         return snapshot.ref.update({isGuest})
       })
   })
 
+function getUser(event, uid) {
+  return event.data.ref.root.child('users/' + uid).once('value')
+}
+
 module.exports = {
+  setGameToUser,
   setUserAsAdmin,
   setUserAsGuest,
 }
--- functions/index.js
+++ functions/index.js
@@ -1,7 +1,7 @@
 const functions = require('firebase-functions')
 const admin = require('./admin')
 const { addAuthUserToDatabase, removeUserFromDatabase } = require('./auth')(admin)
-const { setUserAsAdmin, setUserAsGuest } = require('./database')
+const { setGameToUser, setUserAsAdmin, setUserAsGuest } = require('./database')
 
 const nodeVersions = functions.https.onRequest((req, res) => {
   res.send(process.versions)
@@ -11,6 +11,7 @@ module.exports = {
   addAuthUserToDatabase,
   nodeVersions,
   removeUserFromDatabase,
+  setGameToUser,
   setUserAsAdmin,
   setUserAsGuest,
 }
