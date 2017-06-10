1585051dabf46d943e9cc8ae9f435083363123cf
Ryan Sullivan <ryan@ryansully.com>
2017-06-09 18:21:14 -0700

    Refactor database triggers for user flags

--- functions/database.js
+++ functions/database.js
@@ -17,40 +17,29 @@ const setGameToUser = functions.database.ref('/games/{game_key}')
       })
   })
 
-const setUserAsAdmin = functions.database.ref('/admins/{uid}')
-  .onWrite((event) => {
-    const uid = event.params.uid
-    const isAdmin = event.data.exists() || null
-
-    // set user as admin or remove admin flag if uid is deleted from /admins
-    return getUser(event, uid)
-      .then((snapshot) => {
-        // exit if user is not in database
-        if (!snapshot.exists()) { return }
-
-        console.log('USER ADMIN SET', uid, isAdmin)
-        return snapshot.ref.update({isAdmin})
-      })
-  })
-
-const setUserAsGuest = functions.database.ref('/guests/{uid}')
-  .onWrite((event) => {
-    const uid = event.params.uid
-    const isGuest = event.data.exists() || null
-
-    // set user as guest or remove guest flag if uid is deleted from /guests
-    return getUser(event, uid)
-      .then((snapshot) => {
-        // exit if user is not in database
-        if (!snapshot.exists()) { return }
-
-        console.log('USER GUEST SET', uid, isGuest)
-        return snapshot.ref.update({isGuest})
-      })
-  })
+const setUserAsAdmin = getUserFunction('admins', 'isAdmin', 'USER ADMIN SET')
+const setUserAsGuest = getUserFunction('guests', 'isGuest', 'USER GUEST SET')
 
 function getUser(event, uid) {
   return event.data.ref.root.child('users/' + uid).once('value')
+}
+
+function getUserFunction(path, value, logMessage) {
+  return functions.database.ref(`/${path}/{uid}`)
+    .onWrite((event) => {
+      const uid = event.params.uid
+      const values = {[value]: event.data.exists() || null}
+
+      // set user flag
+      return getUser(event, uid)
+        .then((snapshot) => {
+          // exit if user is not in database
+          if (!snapshot.exists()) { return }
+
+          console.log(logMessage, uid, values)
+        return snapshot.ref.update(values)
+        })
+    })
 }
 
 module.exports = {
