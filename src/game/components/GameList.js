1eb21ac94ac115cc93aba25b95559822beedcf30
Ryan Sullivan <ryan@ryansully.com>
2017-06-18 02:19:52 -0700

    Add last played time to game list

--- src/game/components/GameList.js
+++ src/game/components/GameList.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import { Header, List } from 'semantic-ui-react'
+import distanceInWordsToNow from 'date-fns/distance_in_words_to_now'
 
 const GameList = ({ headerAs = 'h3', headerText, games = [] }) => {
   return (
@@ -12,6 +13,7 @@ const GameList = ({ headerAs = 'h3', headerText, games = [] }) => {
             <List.Item key={game.gameKey}>
               <List.Content>
                 <List.Header as={Link} to={`/game/${game.gameKey}`} content={game.name} />
+                <List.Description content={`Last played ${distanceInWordsToNow(game.lastPlayedAt)} ago`} />
               </List.Content>
             </List.Item>
           ))}
